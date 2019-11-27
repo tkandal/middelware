83a5215bd357ac160a5a49ba8abc41bca5e5a235
Trond Kandal <Trond.Kandal@ntnu.no>
2019-11-27 19:54:03 +0100

    Prettier program name.

--- metrics.go
+++ metrics.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -33,7 +34,7 @@ func PrometheusMetrics(progName string) mux.MiddlewareFunc {
 	requestsDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name: fmt.Sprintf("%s_requests_duration_ms", progName),
-			Help: fmt.Sprintf("%s requests duration ms distribution", progName),
+			Help: fmt.Sprintf("%s requests duration ms distribution", strings.Title(progName)),
 			Buckets: []float64{
 				1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 175, 200, 250, 300, 400, 500,
 				600, 700, 750, 1000, 1250, 1500, 2000, 2500, 3000, 4000, 5000, 7500, 10000, 20000, 30000,
